4dc6e8209f33bf0fca401b1d078dd9c55eecb136
Hannes Blåman <hannes@blaman.com>
2024-03-20 11:51:25 +0100

    Eventpropagering

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@ let counterEl = document.getElementById('counter');
 let counter = 0;
 
 myButton.addEventListener('click', function(event) {
+    event.stopPropagation();
     event.preventDefault();
     counter++;
     counterEl.innerText = counter;
+});
+
+document.body.addEventListener('click', function(event) {
+    document.body.classList.toggle('on');
 });
