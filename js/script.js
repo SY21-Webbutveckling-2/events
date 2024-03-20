11711c1bb3b8d682f155f43b6a9d73c9ae163d32
Hannes Blåman <hannes@blaman.com>
2024-03-20 10:12:45 +0100

    Stoppa standardbeteende

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let counterEl = document.getElementById('counter');
 let counter = 0;
 
 myButton.addEventListener('click', function(event) {
+    event.preventDefault();
     counter++;
     counterEl.innerText = counter;
 });
