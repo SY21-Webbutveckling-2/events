8d0671e9d59b876c006fe3af95afd8352f3ed2f5
Hannes Blåman <hannes@blaman.com>
2024-03-20 10:37:23 +0100

    Boll följer

--- js/ball.js
+++ js/ball.js
@@ -1,6 +1,6 @@
 let ball = document.getElementById('ball');
 
-document.body.addEventListener('click', function(event) {
+document.body.addEventListener('mousemove', function(event) {
     ball.style.top = event.clientY + 'px';
     ball.style.left = event.clientX + 'px';
 });
